9f63be322b84b13c2e2b6d3c6ae4c96561d0e509
Pritam <138203240+hashtagp@users.noreply.github.com>
2024-09-06 12:13:30 +0530

    Update flask.py
    Resolved errors

--- flask.py
+++ flask.py
@@ -1,13 +1,14 @@
 from flask import *
+import random
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-app.secret_key = 'hastagp'
+app.secret_key = 'secret_key'
 
 app.config['MYSQL_HOST'] = 'localhost'  # Replace with your MySQL host
 app.config['MYSQL_USER'] = 'root'  # Replace with your MySQL username
-app.config['MYSQL_PASSWORD'] = ''  # Replace with your MySQL password
+app.config['MYSQL_PASSWORD'] = 'password'  # Replace with your MySQL password
 app.config['MYSQL_DB'] = 'full_stack'  # Replace with your MySQL database name
 
 mysql = MySQL(app)
@@ -30,7 +31,7 @@ def login():
 
                
 
-                query = "SELECT questionid,questions FROM Questions where answer='Not Answered'"
+                query = "SELECT questionid,question FROM Questions where answer='Not Answered'"
                 cur.execute(query)
 
                 results = cur.fetchall()
@@ -39,7 +40,7 @@ def login():
 
                 return render_template('Home.html', values=results, username=username)
 
-    return render_template('test_webpage.html')
+    return render_template('login.html')
 
 @app.route('/register', methods=['POST'])
 def register():
@@ -61,13 +62,13 @@ def register():
         cur.execute("INSERT INTO users (username, password, phone) VALUES (%s, %s, %s)", (username, password, phone))
         mysql.connection.commit()
         cur.close()
-        return render_template('test_webpage.html')
+        return render_template('login.html')
     
 @app.route('/Home')
 def Home():
     cur = mysql.connection.cursor()
     
-    query = "SELECT questionid,questions FROM Questions where answer='Not Answered'"
+    query = "SELECT questionid,question FROM Questions where answer='Not Answered'"
     cur.execute(query)
 
     results = cur.fetchall()
@@ -89,7 +90,7 @@ def answers():
             cur.execute("UPDATE questions SET answer = %s WHERE questionid = %s", (answer, id))
             mysql.connection.commit()
 
-        query = "SELECT questionid,questions FROM Questions where answer='Not Answered'"
+        query = "SELECT questionid,question FROM Questions where answer='Not Answered'"
         cur.execute(query)
 
         results = cur.fetchall()
@@ -100,7 +101,7 @@ def answers():
     
     cur=mysql.connection.cursor()
     
-    query = "SELECT questionid,questions FROM Questions where answer='Not Answered'"
+    query = "SELECT questionid,question FROM Questions where answer='Not Answered'"
     cur.execute(query)
 
     results = cur.fetchall()
@@ -111,24 +112,28 @@ def answers():
 
 @app.route('/qlist', methods=['GET','POST'])
 def qlist():
-    if request.method == 'POST':
-        username=request.form['ID']
-
+    if request.method=='POST':
+        username = request.form['username']
         cur = mysql.connection.cursor()
-        cur.execute("select questionid,questions from questions where username=%s",(username,))
+        cur.execute("select questionid,question from questions where username=%s",(username,))
         res = cur.fetchall()
-        return render_template('MyQuestions.html', values=res)
+        print(res)
+    return render_template('MyQuestions.html', values = res, username = username)
     
-@app.route('/qinsert', methods=['POST'])
+@app.route('/qinsert', methods=['GET' , 'POST'])
 def qinsert():
+    username = request.args.get('value')
     if request.method == 'POST':
         question = request.form['question']
-        username = request.form['id']
-
+        username = request.form['username']
         cur = mysql.connection.cursor()
-        cur.execute('insert into questions(questions,username,answer) values(%s,%s,%s)',(question,username,"Not Answered"))
+        if (not question.isspace() and len(question)!=0):
+            cur.execute('insert into questions(question,username) values(%s,%s)',(question,username))
+            mysql.connection.commit()
+        else:
+            return render_template("Questions.html",error="Questions cannot contain only spaces!!")
 
-        query = "SELECT questionid,questions FROM Questions where answer='Not Answered'"
+        query = "SELECT questionid,question FROM Questions where answer='Not Answered'"
         cur.execute(query)
 
         results = cur.fetchall()
@@ -139,7 +144,7 @@ def qinsert():
     
     cur = mysql.connection.cursor()
     
-    query = "SELECT questionid,questions FROM Questions where answer='Not Answered'"
+    query = "SELECT questionid,question FROM Questions where answer='Not Answered'"
     cur.execute(query)
 
     results = cur.fetchall()
@@ -168,8 +173,8 @@ def MyQuestions():
 def answer():
     value = request.args.get('value')
     cur=mysql.connection.cursor()
-    cur.execute('select questionid,questions from questions where questionid=%s',(value,))
-    res=cur.fetchall()
+    cur.execute('select questionid,question from questions where questionid=%s',(value))
+    res=cur.fetchone()
 
     return render_template('answer.html', value=res)
 
@@ -177,14 +182,20 @@ def answer():
 def answerdisp():
     value = request.args.get('res')
     cur=mysql.connection.cursor()
-    cur.execute('select questionid,questions,answer from questions where questionid=%s',(value,))
-    res=cur.fetchall()
+    cur.execute('select questionid,question,answer from questions where questionid=%s',(value))
+    res=cur.fetchone()
 
     return render_template('answerdisp.html', value=res)
 
 @app.route('/Questions.html')
 def Questions():
     return render_template('Questions.html')
+
+@app.route('/Profile.html')
+def Profile():
+    return render_template('Profile.html')
+
+
 
 
 if __name__ == '__main__':
